3a0ff6419989463d10677e985103b46437584328
monk-haus <hello@monk.haus>
2026-02-11 23:29:49 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
+  reactCompiler: true, 
+  
   images: {
     unoptimized: true,
   },
